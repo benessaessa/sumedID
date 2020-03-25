d2840c632bc8c1054853db2246d78b07bfe7247d
mohamed <mohamed.essa.elderwy@gmail.com>
2020-03-25 17:00:47 +0200

    admin

--- js/file.js
+++ js/file.js
@@ -5,6 +5,27 @@
         $('.accountC').click(function(){
             $('.mAcount').toggle();
 });
+$("#color_me").change(function(){
+  var color = $("option:selected", this).attr("class");
+  $("#color_me").attr("class", color);
+});
+$("#color_me2").change(function(){
+  var color = $("option:selected", this).attr("class");
+  $("#color_me2").attr("class", color);
+});
+$("#color_me3").change(function(){
+  var color = $("option:selected", this).attr("class");
+  $("#color_me3").attr("class", color);
+});
+$("#color_me4").change(function(){
+  var color = $("option:selected", this).attr("class");
+  $("#color_me4").attr("class", color);
+});
+$("#color_me5").change(function(){
+  var color = $("option:selected", this).attr("class");
+  $("#color_me5").attr("class", color);
+});
+
 // $('.addSite').click(function(){
 //   $('.hideGroup').toggle();
 // })
