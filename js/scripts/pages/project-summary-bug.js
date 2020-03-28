e89303c73b4c479c86904357933250ae9e0f41d3
mohamed <mohamed.essa.elderwy@gmail.com>
2020-03-28 14:35:36 +0200

    edit updaes

--- js/scripts/pages/project-summary-bug.js
+++
@@ -1,8 +0,0 @@
-/*=========================================================================================
-    File Name: project-summary-task.js
-    Description: Project Summary Page JS
-    ----------------------------------------------------------------------------------------
-    Item Name: Modern Admin - Clean Bootstrap 4 Dashboard HTML Template
-    Author: GeeksLabs
-    Author URL: http://www.themeforest.net/user/geekslabs
-==========================================================================================*/
